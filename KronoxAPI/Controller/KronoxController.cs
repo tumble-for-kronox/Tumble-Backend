95e5e9708f6f970c3728b36fbf2bd9dd1f974bb5
Lasse Poulsen <work.lasse.face@gmail.com>
2022-06-23 01:56:11 +0200

    Finish KronoxController connetions to Kronox website.
    All GETTER functions should now work with logged in and not logged in instances

--- KronoxAPI/Controller/KronoxController.cs
+++ KronoxAPI/Controller/KronoxController.cs
@@ -15,7 +15,8 @@ namespace KronoxAPI.Controller
     /// </summary>
     public static class KronoxController
     {
-        static readonly HttpClient client = new HttpClient();
+        static HttpClientHandler clientHandler = new HttpClientHandler();
+        static readonly HttpClient client = new HttpClient(clientHandler);
 
         /// <summary>
         /// Fetch a schedule from Kronox's database, that starts at <paramref name="startDate"/> and goes 6 months forward.
@@ -34,11 +35,10 @@ namespace KronoxAPI.Controller
             string parsedDate = startDate.HasValue ? startDate.Value.ToString("yyyy-MM-dd") : "idag";
             string uri = $"https://{schoolUrl}/setup/jsp/SchemaICAL.ics?startDatum={parsedDate}&intervallTyp=m&intervallAntal=6&sprak=SV&sokMedAND=true&forklaringar=true&resurser={scheduleId}";
 
-            var request = new HttpRequestMessage(HttpMethod.Get, uri);
+            using var request = new HttpRequestMessage(HttpMethod.Get, uri);
+            if (sessionToken != null) request.Headers.Add("Cookie", $"JSESSIONID={sessionToken}");
 
-            if (sessionToken != null) request.Headers.Add("cookies", $"JSESSIONID={sessionToken};");
-
-            var response = await client.SendAsync(request);
+            HttpResponseMessage response = await client.SendAsync(request);
             response.EnsureSuccessStatusCode();
             return await response.Content.ReadAsStringAsync();
         }
@@ -56,26 +56,73 @@ namespace KronoxAPI.Controller
         /// <returns></returns>
         public static async Task<string> GetProgrammes(string searchQuery, string schoolUrl, string? sessionToken)
         {
-            string uri = $"https://kronox.hkr.se/ajax/ajax_sokResurser.jsp?sokord={searchQuery}&startDatum=idag&slutDatum=&intervallTyp=m&intervallAntal=6";
+            Uri uri = new($"https://{schoolUrl}/ajax/ajax_sokResurser.jsp?sokord={searchQuery}&startDatum=idag&slutDatum=&intervallTyp=m&intervallAntal=6");
 
-            string responseBody = await client.GetStringAsync(uri);
-            return responseBody;
+            // Perform web request
+            using var request = new HttpRequestMessage(new HttpMethod("GET"), uri);
+            if (sessionToken != null) request.Headers.Add("Cookie", $"JSESSIONID={sessionToken}");
+            HttpResponseMessage response = await client.SendAsync(request);
+            response.EnsureSuccessStatusCode();
+
+            return await response.Content.ReadAsStringAsync();
         }
 
-        public static async Task<string> GetUserEvents(string sessionToken)
+        /// <summary>
+        /// Https fetch to Kronox, getting the HTML page with <see cref="Model.User.UserEvent"/> information.
+        /// </summary>
+        /// <param name="schoolUrl"></param>
+        /// <param name="sessionToken"></param>
+        /// <returns><see cref="string"/> HTML page, which carries <see cref="Model.User.UserEvent"/> data.</returns>
+        public static async Task<string> GetUserEvents(string schoolUrl, string sessionToken)
         {
-            return "";
+            Uri uri = new($"https://{schoolUrl}/aktivitetsanmalan.jsp");
+
+            // Perform web request
+            using var request = new HttpRequestMessage(new HttpMethod("GET"), uri);
+            request.Headers.Add("Cookie", $"JSESSIONID={sessionToken}");
+            HttpResponseMessage response = await client.SendAsync(request);
+            response.EnsureSuccessStatusCode();
+
+            return await response.Content.ReadAsStringAsync();
         }
 
+        /// <summary>
+        /// Login a user, using the <paramref name="username"/> and <paramref name="password"/> supplied.
+        /// </summary>
+        /// <param name="username"></param>
+        /// <param name="password"></param>
+        /// <param name="schoolUrl"></param>
+        /// <returns><see cref="string"/> containing the login session token.</returns>
         public static async Task<string> Login(string username, string password, string schoolUrl)
         {
-            using var request = new HttpRequestMessage(new HttpMethod("POST"), $"https://{schoolUrl}/login_do.jsp");
+            // Create CookieContainer to store eventual response cookies in
+            CookieContainer cookies = new();
+            // Make sure the client has a new, empty CookieContainer
+            clientHandler.CookieContainer = cookies;
+
+            Uri uri = new($"https://{schoolUrl}/login_do.jsp");
+
+            // Perform web request
+            using var request = new HttpRequestMessage(new HttpMethod("POST"), uri);
             request.Content = new StringContent($"username={WebUtility.UrlEncode(username)}&password={WebUtility.UrlEncode(password)}");
             request.Content.Headers.ContentType = MediaTypeHeaderValue.Parse("application/x-www-form-urlencoded");
-
-            var response = await client.SendAsync(request);
+            HttpResponseMessage response = await client.SendAsync(request);
+            
+            // Handle response, making sure it's good and preparing to strip session cookie
             response.EnsureSuccessStatusCode();
-            return await response.Content.ReadAsStringAsync();
+            string sessionToken = string.Empty;
+
+            // Fetch cookies as iterable from CookieContainer
+            IEnumerable<Cookie> responseCookies = cookies.GetCookies(uri).Cast<Cookie>();
+            foreach (Cookie cookie in responseCookies)
+            {
+                if (cookie.Name == "JSESSIONID") sessionToken = cookie.Value;
+                // Make sure all the current cookies are expired, so there's no bleed between http requests on the same client
+                cookie.Expired = true;
+            }
+
+            // Return the relevant cookie value (login session token)
+            return sessionToken;
         }
     }
 }
--- TestingConsole/Program.cs
+++ TestingConsole/Program.cs
@@ -2,4 +2,6 @@
 
 // See https://aka.ms/new-console-template for more information
 
-Console.WriteLine(KronoxController.Login("lasse_koordt_rosenkrans.poulsen0003@stud.hkr.se", "oUPJA4j@iocd$dp", "schema.hkr.se").Result);
+string sessionToken = KronoxController.Login("lasse_koordt_rosenkrans.poulsen0003@stud.hkr.se", "oUPJA4j@iocd$dp", "schema.hkr.se").Result;
+
+Console.WriteLine(KronoxController.GetUserEvents("schema.hkr.se", sessionToken).Result);
